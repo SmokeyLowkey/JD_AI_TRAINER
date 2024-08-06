666ac44ea3f42c21bfdd3169885ca7a9e8d9f13c
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-08-06 01:46:44 -0600

    modified:   API/API/__pycache__/settings.cpython-312.pyc 	modified:   API/API/settings.py 	modified:   API/data/management/__pycache__/__init__.cpython-312.pyc 	modified:   API/debug.log 	modified:   API/requirements.txt

--- API/API/settings.py
+++ API/API/settings.py
@@ -98,8 +98,12 @@ CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGIN').split(',')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
