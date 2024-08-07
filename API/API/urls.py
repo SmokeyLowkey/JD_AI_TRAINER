5fc1894371b0493d429180e6a6ed0437a43eab24
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-08-06 23:28:39 -0600

    modified:   API/API/urls.py

--- API/API/urls.py
+++ API/API/urls.py
@@ -1,5 +1,7 @@
 # urls.py
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from data.admin import PartAdmin
@@ -17,4 +19,4 @@ urlpatterns = [
     path('api/avatar_presigned_url', avatar_presigned_url, name='avatar_presigned_url'),
     path('api/animation_presigned_url', animation_presigned_url, name='animation_presigned_url'),
     path('api/get-csrf-token/', get_csrf_token, name='get_csrf_token')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
